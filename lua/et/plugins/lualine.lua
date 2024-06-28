9d47fcf50ebc9420e243083e2fe6835f46a9cccb
Tapio Holopainen <e.tapio.holopainen@gmail.com>
2024-06-28 13:33:28 +0300

    lualine, whichkey, telescope changes

--- lua/et/plugins/lualine.lua
+++ lua/et/plugins/lualine.lua
@@ -1,27 +1,36 @@
+local function treesitter_active()
+  return vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()]
+end
+
+local tree_sitter = {
+  cond = treesitter_active(),
+  icon = require("nvim-web-devicons").get_icon_by_filetype(".bashrc", {}),
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local lualine = require "lualine"
+    local lazy_status = require "lazy.status" -- to configure lazy pending updates count
 
     -- configure lualine with modified theme
-    lualine.setup({
+    lualine.setup {
       options = {
         -- theme = "catppuccin",
         theme = "auto",
       },
       sections = {
         lualine_a = {
-          "mode"
+          "mode",
         },
         lualine_b = {
-          'branch',
-          'diff',
-          'diagnostics'
+          "branch",
+          "diff",
+          "diagnostics",
         },
         lualine_c = {
-          'filename'
+          "filename",
         },
         lualine_x = {
           {
@@ -33,8 +42,11 @@ return {
           { "fileformat" },
           { "filetype" },
         },
+        lualine_z = {
+          tree_sitter,
+        },
       },
       extensions = { "neo-tree", "lazy" },
-    })
+    }
   end,
 }
--- lua/et/plugins/telescope.lua
+++ lua/et/plugins/telescope.lua
@@ -16,7 +16,7 @@ local function find_git_root()
   -- Find the Git root directory from the current file's path
   local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
   if vim.v.shell_error ~= 0 then
-    print("Not a git repository. Searching on current working directory")
+    print "Not a git repository. Searching on current working directory"
     return cwd
   end
   return git_root
@@ -26,9 +26,9 @@ end
 local function live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
-    require("telescope.builtin").live_grep({
+    require("telescope.builtin").live_grep {
       search_dirs = { git_root },
-    })
+    }
   end
 end
 
@@ -49,7 +49,7 @@ return {
         --       refer to the README for telescope-fzf-native for more instructions.
         build = "make",
         cond = function()
-          return vim.fn.executable("make") == 1
+          return vim.fn.executable "make" == 1
         end,
       },
       {
@@ -81,17 +81,17 @@ return {
       map("n", "<leader>fg", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
       map("n", "<leader>s/", function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
           winblend = 10,
           previewer = false,
-        }))
+        })
       end, { desc = "[/] Fuzzily search in current buffer" })
 
       local function telescope_live_grep_open_files()
-        require("telescope.builtin").live_grep({
+        require("telescope.builtin").live_grep {
           grep_open_files = true,
           prompt_title = "Live Grep in Open Files",
-        })
+        }
       end
       local function telescope_live_grep_args()
         require("telescope").extensions.live_grep_args.live_grep_args()
@@ -106,23 +106,27 @@ return {
       map("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
       map("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
 
-      require("telescope").setup({
+      require("telescope").setup {
         extensions = {
           ["ui-select"] = {},
           live_grep_args = {
             auto_quoting = false, -- enable/disable auto-quoting
             -- define mappings, e.g.
-            mappings = {          -- extend mappings
+            mappings = { -- extend mappings
               i = {
                 ["<C-k>"] = lga_actions.quote_prompt(),
-                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-                ["<C-j>"] = lga_actions.quote_prompt({ postfix = " --iglob '**/src/main/java/**'" }),
-                ["<C-t>"] = lga_actions.quote_prompt({ postfix = " --iglob '**/src/test/java/**'" }),
+                ["<C-i>"] = lga_actions.quote_prompt { postfix = " --iglob " },
+                ["<C-j>"] = lga_actions.quote_prompt { postfix = " --iglob '**/src/main/java/**'" },
+                ["<C-t>"] = lga_actions.quote_prompt { postfix = " --iglob '**/src/test/java/**'" },
               },
             },
           },
         },
         defaults = {
+          layout_strategy = "vertical",
+          layout_config = {
+            height = 0.95,
+          },
           mappings = {
             i = {
               ["<C-u>"] = false,
@@ -130,7 +134,7 @@ return {
             },
           },
         },
-      })
+      }
       -- Enable telescope fzf native, if installed
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "live_grep_args")
