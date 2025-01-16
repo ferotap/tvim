13853d95b9197822e72aff208c564624515df6d4
Tapio Holopainen <e.tapio.holopainen@gmail.com>
2025-01-16 06:15:10 +0200

    fzf-lua tuning

--- lua/et/plugins/fzf_lua.lua
+++ lua/et/plugins/fzf_lua.lua
@@ -1,49 +1,48 @@
+local map = vim.keymap.set
 return {
   "ibhagwan/fzf-lua",
-  -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- calling `setup` is optional for customization
-    local fzf = require("fzf-lua")
-    local map = vim.keymap.set
-    fzf.setup({
-      keys = {
-        -- buffers and files
-        map("n", "<leader>fb", function() fzf.buffers({}) end, { desc = "Buffers (FZF)" }),
-        map("n", "<leader>ff", function() fzf.files({}) end, { desc = "Files (FZF)" }),
-        map("n", "<leader>fo", function() fzf.oldfiles({}) end, { desc = "Recent Files (FZF)" }),
-        map("n", "<leader>fc", function() fzf.quickfix({}) end, { desc = "Quickfix List (FZF)" }),
-        map("n", "<leader>fC", function() fzf.quickfix_stack({}) end, { desc = "Quickfix Stack (FZF)" }),
-        map("n", "<leader>fl", function() fzf.loclist({}) end, { desc = "Loclist List (FZF)" }),
-        map("n", "<leader>fL", function() fzf.loclist_stack({}) end, { desc = "Loclist Stack (FZF)" }),
-        map("n", "<leader>ft", function() fzf.treesitter({}) end, { desc = "Treesitter Symbols(FZF)" }),
+  keys = {
+    -- buffers and files
+    map("n", "<leader>fb", function() require("fzf-lua").buffers({}) end, { desc = "Buffers (FZF)" }),
+    map("n", "<leader>ff", function() require("fzf-lua").files({}) end, { desc = "Files  (FZF)" }),
+    map("n", "<leader>fo", function() require("fzf-lua").oldfiles({}) end, { desc = "Recent Files (FZF)" }),
+    map("n", "<leader>fc", function() require("fzf-lua").quickfix({}) end, { desc = "Quickfix List (FZF)" }),
+    map("n", "<leader>fC", function() require("fzf-lua").quickfix_stack({}) end, { desc = "Quickfix Stack (FZF)" }),
+    map("n", "<leader>fl", function() require("fzf-lua").loclist({}) end, { desc = "Loclist List (FZF)" }),
+    map("n", "<leader>fL", function() require("fzf-lua").loclist_stack({}) end, { desc = "Loclist Stack (FZF)" }),
+    map("n", "<leader>ft", function() require("fzf-lua").treesitter({}) end, { desc = "Treesitter Symbols(FZF)" }),
 
-        -- git
-        map("n", "<leader>gc", function() fzf.git_commits({}) end, { desc = "git commit log (project) (FZF)" }),
-        map("n", "<leader>gC", function() fzf.git_bcommits({}) end, { desc = "git commit log (buffer) (FZF)" }),
-        map("n", "<leader>gf", function() fzf.ls_files({}) end, { desc = "git ls-files (FZF)" }),
-        map("n", "<leader>gs", function() fzf.git_status({}) end, { desc = "git status (FZF)" }),
-        map("n", "<leader>gb", function() fzf.git_blame({}) end, { desc = "git blame (FZF)" }),
-        map("n", "<leader>gt", function() fzf.git_tags({}) end, { desc = "git tags (FZF)" }),
+    -- git
+    map("n", "<leader>gc", function() require("fzf-lua").git_commits({}) end, { desc = "git commit log (project) (FZF)" }),
+    map("n", "<leader>gC", function() require("fzf-lua").git_bcommits({}) end, { desc = "git commit log (buffer) (FZF)" }),
+    map("n", "<leader>gf", function() require("fzf-lua").ls_files({}) end, { desc = "git ls-files (FZF)" }),
+    map("n", "<leader>gs", function() require("fzf-lua").git_status({}) end, { desc = "git status (FZF)" }),
+    map("n", "<leader>gb", function() require("fzf-lua").git_blame({}) end, { desc = "git blame (FZF)" }),
+    map("n", "<leader>gt", function() require("fzf-lua").git_tags({}) end, { desc = "git tags (FZF)" }),
 
-        -- search
-        map("n", "<leader>sg", function() fzf.live_grep({}) end, { desc = "Live Grep (FZF)" }),
-        map("n", "<leader>sG", function() fzf.live_grep({}) end, { desc = "Live Grep --glob (FZF)" }),
-        map("n", "<leader>sr", function() fzf.live_grep_resume({}) end, { desc = "Resume Search Pattern (FZF)" }),
-        map("n", "<leader>sw", function() fzf.cword({}) end, { desc = "Search Word Under Cursor (FZF)" }),
-        map("n", "<leader>sW", function() fzf.cWORD({}) end, { desc = "Search WORD Under Cursor (FZF)" }),
-        map("n", "<leader>sm", function() fzf.manpages({}) end, { desc = "Man pages (FZF)" }),
-        map("n", "<leader>sh", function() fzf.help_tags({}) end, { desc = "Man pages (FZF)" }),
-        map("n", "<leader>sk", function() fzf.keymaps({}) end, { desc = "Key mappings (FZF)" }),
+    -- search
+    map("n", "<leader>sg", function() require("fzf-lua").live_grep({}) end, { desc = "Live Grep (FZF)" }),
+    map("n", "<leader>sG", function() require("fzf-lua").live_grep({}) end, { desc = "Live Grep --glob (FZF)" }),
+    map("n", "<leader>sr", function() require("fzf-lua").live_grep_resume({}) end,
+      { desc = "Resume Search Pattern (FZF)" }),
+    map("n", "<leader>sw", function() require("fzf-lua").cword({}) end, { desc = "Search Word Under Cursor (FZF)" }),
+    map("n", "<leader>sW", function() require("fzf-lua").cWORD({}) end, { desc = "Search WORD Under Cursor (FZF)" }),
+    map("n", "<leader>sm", function() require("fzf-lua").manpages({}) end, { desc = "Man pages (FZF)" }),
+    map("n", "<leader>sh", function() require("fzf-lua").help_tags({}) end, { desc = "Man pages (FZF)" }),
+    map("n", "<leader>sk", function() require("fzf-lua").keymaps({}) end, { desc = "Key mappings (FZF)" }),
 
-        -- tags
-        map("n", "<leader>tf", function() fzf.tags({}) end, { desc = "Search Project Tgs (FZF)" }),
-        map("n", "<leader>tb", function() fzf.btags({}) end, { desc = "Search Buffer Tags (FZF)" }),
+    -- tags
+    map("n", "<leader>tf", function() require("fzf-lua").tags({}) end, { desc = "Search Project Tgs (FZF)" }),
+    map("n", "<leader>tb", function() require("fzf-lua").btags({}) end, { desc = "Search Buffer Tags (FZF)" }),
 
-        -- --LSP mappings in plugins/lsp.lua
-        -- misc
-        map("n", "<leader>tb", function() fzf.tags({}) end, { desc = "Search Buffer Tags (FZF)" }),
-      },
-    })
-  end
+    -- --LSP mappings in plugins/lsp.lua
+    -- misc
+    map("n", "<leader>tb", function() require("fzf-lua").tags({}) end, { desc = "Search Buffer Tags (FZF)" }),
+  },
+  opts = {
+    files = {
+      cwd_prompt = false,
+    },
+  }
 }
