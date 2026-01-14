fb9dcfcfa5f50430477f8daed112b0997543a3b8
Tapio Holopainen <e.tapio.holopainen@gmail.com>
2026-01-14 09:40:31 +0200

    quick fix for treesitter breakin changes
    Now missing the list of "needed" parsers

--- lua/et/plugins/treesitter.lua
+++ lua/et/plugins/treesitter.lua
@@ -1,67 +1,85 @@
-local M = {
+return {
   -- Highlight, edit, and navigate code
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
   },
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
+
+  {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "main",
+  init = function()
+    -- Disable entire built-in ftplugin mappings to avoid conflicts.
+    -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+    vim.g.no_plugin_maps = true
+
+    -- Or, disable per filetype (add as you like)
+    -- vim.g.no_python_maps = true
+    -- vim.g.no_ruby_maps = true
+    -- vim.g.no_rust_maps = true
+    -- vim.g.no_go_maps = true
+  end,
+  config = function()
+    -- put your config here
+  end,
+}
 }
 
-function M.config()
-  local configs = require("nvim-treesitter.configs")
-  configs.setup({
-    modules = {},
-    sync_install = false,
-    auto_install = true,
-    ignore_install = {},
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-    textobjects = {
-      move = {
-        enable = true,
-        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-      },
-    },
-    ensure_installed = {
-      "bash",
-      "c",
-      "cmake",
-      "diff",
-      "dockerfile",
-      "graphql",
-      "html",
-      "http",
-      "jq",
-      "java",
-      "javascript",
-      "json",
-      "jsonc",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "sql",
-      "query",
-      "toml",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-  })
-end
-
-return M
+-- function M.config()
+--   local config = require("nvim-treesitter.config")
+--   config.setup({
+--     modules = {},
+--     sync_install = false,
+--     auto_install = true,
+--     ignore_install = {},
+--     highlight = { enable = true },
+--     indent = { enable = true },
+--     incremental_selection = {
+--       enable = true,
+--       keymaps = {
+--         init_selection = "<C-space>",
+--         node_incremental = "<C-space>",
+--         scope_incremental = false,
+--         node_decremental = "<bs>",
+--       },
+--     },
+--     textobjects = {
+--       move = {
+--         enable = true,
+--         goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+--         goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+--         goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+--         goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+--       },
+--     },
+--     ensure_installed = {
+--       "bash",
+--       "c",
+--       "cmake",
+--       "diff",
+--       "dockerfile",
+--       "graphql",
+--       "html",
+--       "http",
+--       "jq",
+--       "java",
+--       "javascript",
+--       "json",
+--       "jsonc",
+--       "lua",
+--       "markdown",
+--       "markdown_inline",
+--       "python",
+--       "sql",
+--       "query",
+--       "toml",
+--       "typescript",
+--       "vim",
+--       "vimdoc",
+--       "yaml",
+--     },
+--   })
+-- end
+--
+-- return M
